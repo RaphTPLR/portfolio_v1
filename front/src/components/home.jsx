1868438afc897b8c7164708c49c3acd7d9a37d3b
RaphTPLR <102389757+RaphTPLR@users.noreply.github.com>
2024-01-10 12:59:43 +0100

    feat: navbar

--- front/src/components/home.jsx
+++ front/src/components/home.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
 import '../style/home.scss';
 
 export default function Home() {
@@ -8,9 +7,9 @@ export default function Home() {
 
     useEffect(() => {
         fetch('src/data/data.json')
-            .then(response => response.json())
-            .then(data => setDonnees(data))
-            .catch(error => console.error('Erreur de chargement des données :', error));
+        .then(response => response.json())
+        .then(data => setDonnees(data))
+        .catch(error => console.error('Erreur de chargement des données :', error));
     }, []);
 
     const handleDivHover = (index) => {
@@ -23,31 +22,67 @@ export default function Home() {
                 <p>RR</p>
             </div>
             <div className="content">
-                <div className="navbar"></div>
-                <div className="container">
+                <div className="navbar">
                     {donnees.length > 0 && (
                         <React.Fragment>
-                            {Array.from({ length: donnees.length }, (_, index) => (
+                            {donnees.map((item, index) => (
                                 <div
-                                    key={donnees[index].id}
-                                    onMouseOver={() => handleDivHover(index)}
-                                    className={`image-div ${imageAffichee === index ? 'selected' : 'unselected'}`}
-                                >
-                                    {imageAffichee === index && (
-                                        <img src={donnees[index].images[0]} alt={`Image ${donnees[index].id}`} />
-                                    )}
+                                    className={`navbar-content ${
+                                        imageAffichee === index ? 'selected' : ''
+                                    }`}
+                                    key={item.id}
+                                    >
+                                    <div className="indice"></div>
+                                    <div className="name">
+                                        {imageAffichee === index ? <p>{item.name}</p> : ""}
+                                    </div>
                                 </div>
                             ))}
                         </React.Fragment>
                     )}
                 </div>
+                <div className="container">
+                {donnees.length > 0 && (
+                    <React.Fragment>
+                    {donnees.map((item, index) => (
+                        <div
+                        key={item.id}
+                        onMouseOver={() => handleDivHover(index)}
+                        className={`image-div ${
+                            imageAffichee === index ? 'selected' : 'unselected'
+                        }`}
+                        style={
+                            imageAffichee !== index
+                            ? {
+                                backgroundImage: `url(${item.images[0]})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                filter: 'grayscale(70%)',
+                                }
+                            : null
+                        }
+                        >
+                        {imageAffichee === index && (
+                            <img
+                            src={item.images[0]}
+                            alt={`Image ${item.id}`}
+                            />
+                        )}
+                        </div>
+                    ))}
+                    </React.Fragment>
+                )}
+                </div>
                 <div className="name-project">
                     {imageAffichee !== null && donnees[imageAffichee] !== undefined && (
-                        <h2>{donnees[imageAffichee].nom}</h2>
+                        <h2>{donnees[imageAffichee].name}</h2>
                     )}
                 </div>
             </div>
             <div className="footer fontspe">
+                {imageAffichee !== null && donnees[imageAffichee] !== undefined && (
+                    <h2>{donnees[imageAffichee].id}/{donnees.length}</h2>
+                )}
                 <p>V1</p>
             </div>
         </div>
