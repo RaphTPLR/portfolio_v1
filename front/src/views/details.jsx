c41d117df5d534f1ee9249bb091d24974bfc2169
RaphTPLR <102389757+RaphTPLR@users.noreply.github.com>
2024-01-15 15:12:38 +0100

    fix: checkpoint Details + Home no bug

--- front/src/views/details.jsx
+++ front/src/views/details.jsx
@@ -4,9 +4,42 @@ import { Link } from "react-router-dom"
 
 
 export default function Details() {
+    const [url, setUrl] = useState();
+    const [donnees, setDonnees] = useState([]);
+    const [item, setItem] = useState();
+
+    useEffect(() => {
+      fetch("src/data/data.json")
+        .then((response) => response.json())
+        .then((data) => setDonnees(data))
+        .catch((error) =>
+          console.error("Erreur de chargement des données :", error)
+        );
+    }, []);
+
+    useEffect(() => {
+      const cheminURL = window.location.pathname;
+      const partieApresDomaine = cheminURL.replace(/^\/+|\/+$/g, "");
+      setUrl("/" + partieApresDomaine);
+    }, []);
+
+    useEffect(() => {
+      if (donnees.length > 0) {
+        for (let i = 0; i < donnees.length; i++) {
+          if (url === donnees[i].path) {
+            setItem(donnees[i]);
+            break;
+          }
+        }
+      }
+    }, [url, donnees, item]);
     return (
       <div className="details">
-        
+        {item ? (
+            <p>{item.name}</p>
+          ) : (
+            <p></p>
+          )}
       </div>
     )
 }
